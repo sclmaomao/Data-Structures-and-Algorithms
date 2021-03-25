aa10fed1ebc58842a653b50c0846eff3f8e8fd5f
sclmaomao <csun64@asu.edu>
2021-03-25 00:42:44 -0500

    Create MaximumSubarray.py

---
+++ Array/MaximumSubarray.py
@@ -0,0 +1,16 @@
+class Solution:
+    """
+    @param nums: A list of integers
+    @return: A integer indicate the sum of max subarray
+    """
+    def maxSubArray(self, nums):
+        #prefix_sum记录前i个数的和，max_Sum记录全局最大值，min_Sum记录前i个数中0-k的最小值
+        min_sum, max_sum = 0, -sys.maxsize
+        prefix_sum = 0
+        
+        for num in nums:
+            prefix_sum += num
+            max_sum = max(max_sum, prefix_sum - min_sum)
+            min_sum = min(min_sum, prefix_sum)
+            
+        return max_sum

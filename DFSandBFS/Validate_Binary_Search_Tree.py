d621d023eb14d0143921157dba71c55c84c46ec5
sclmaomao <csun64@asu.edu>
2021-04-20 23:40:02 -0500

    Create Validate_Binary_Search_Tree.py

---
+++ DFSandBFS/Validate_Binary_Search_Tree.py
@@ -0,0 +1,20 @@
+class Solution:
+    """
+    @param root: The root of binary tree.
+    @return: True if the binary tree is BST, or false
+    """
+    def isValidBST(self, root):
+        self.lastVal = None
+        self.isBST = True
+        self.validate(root)
+        return self.isBST
+
+    def validate(self, root):
+        if root is None:
+            return
+        self.validate(root.left)
+        if self.lastVal is not None and self.lastVal >= root.val:
+            self.isBST = False
+            return
+        self.lastVal = root.val
+        self.validate(root.right)
